b4b889a0300c08d05904de12808a60fc21e2dd1e
도훈 <money200won@gmail.com>
2017-12-13 19:22:31 +0900

    list 메소드 연습

---
+++ samplePrj/src/Practice/P805.java
@@ -0,0 +1,20 @@
+package Practice;
+
+import java.util.ArrayList;
+
+public class P805 {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0 ; i<10 ; i++)
+			list.add(i);
+		
+	list.forEach(i->System.out.print(i+","));
+	System.out.println();
+	
+	list.removeIf(x -> x%2==0 || x%3==0);
+	System.out.println(list);
+	
+	list.replaceAll(i->i*10);
+	System.out.println(list);
+	}
+}
